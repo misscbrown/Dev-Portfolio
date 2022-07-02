8f2b497c866b96eeebb436a3bd214999bbd29904
cee breezy <djmisscbrown@hotmail.com>
2022-07-02 15:21:15 +0100

    Added on click functions for buttons

--- Public/Javascript/index.js
+++ Public/Javascript/index.js
@@ -18,12 +18,28 @@ document.getElementById("hack-btn").onclick = function () {
     location.href = "https://github.com/Farhiya1/Hackathon-Suite";
 }
 
+document.getElementById("hack-deploy-btn").onclick = function () {
+    location.href = "https://salty-woodland-88670.herokuapp.com/"
+}
+
 document.getElementById("repo-btn").onclick = function () {
     location.href = "https://github.com/LanguageBytes/Junior-Dev-Job-Search-Engine";
 }
 
+document.getElementById("dev-deploy-btn").onclick = function () {
+    location.href = "https://languagebytes.github.io/Junior-Dev-Job-Search-Engine/";
+}
+
 document.getElementById("readme-btn").onclick = function () {
     location.href = "https://github.com/misscbrown/Readme-Creator";
+}
+
+document.getElementById("weather-btn").onclick = function () {
+    location.href = "https://github.com/misscbrown/weather-forecast-app";
+}
+
+document.getElementById("dep-weather-btn").onclick = function () {
+    location.href = "https://misscbrown.github.io/weather-forecast-app/";
 }
 
 document.getElementById("ecommerce-btn").onclick = function () {
@@ -34,4 +50,6 @@ document.getElementById("scheduler-btn").onclick = function () {
     location.href = "https://github.com/misscbrown/Work-Day-Scheduler";
 }
 
-
+document.getElementById("dep-scheduler-btn").onclick = function () {
+    location.href = "https://misscbrown.github.io/Work-Day-Scheduler/Develop/"
+}
